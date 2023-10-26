986805b9d2c0f9f3d7cd0f454b8dab1bdef40103
yarrobong <oneboardsholi@gmail.com>
2023-10-26 10:34:37 +0500

    Добавьте файлы проекта.

---
+++ DeletePovtorHomeWork/Program.cs
@@ -0,0 +1,10 @@
+﻿string str = Console.ReadLine();
+string result = String.Empty;
+for (int i = 0; i < str.Length; i++)
+{
+    if (!result.Contains(str[i]))
+    {
+        result = result + str[i];
+    }
+}
+Console.WriteLine(result);
